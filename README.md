e3db72e2b4c92adf7056c40b70675b0aed6bdb6e
Alx <a.sergienko@hadrout.com>
2016-01-04 14:27:46 +0300

    First

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # http-async
 node.js es6 http wrapper
+
+## Install
+`npm install --save https://github.com/Alexnder/http-async.git`
+
+## Usage
+```
+var httpAsync = require('http-async');
+
+httpAsync.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0")
+.then(function(data) {
+  return JSON.parse(data);
+})
+.then(function(result) {
+  console.log(result.name, result.main.temp);
+});
+
+httpAsync.downloadFile("result.json", "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0");
+```
---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var http = require('http');
+var fs = require('fs');
+
+function httpGet(url) {
+  return new Promise(function (resolve, reject) {
+    http.get(url, function(res) {
+      var body = '';
+      res.on('data', function(data){
+        body += data;
+      });
+      res.on('end', function() {
+        resolve(body);
+      });
+    })
+    .on('error', function(e) {
+      reject(e);
+    });
+  });
+}
+
+function downloadFile(filename, url) {
+  return new Promise(function (resolve, reject) {
+    var file = fs.createWriteStream(filename);
+    http.get(url, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close(resolve);
+      });
+    })
+    .on('error', function(e) {
+      if (fs.exists(filename)) {
+        fs.unlink(filename);
+      }
+      reject(e);
+    });
+  });
+}
+
+exports.get = httpGet;
+exports.downloadFile = downloadFile;
